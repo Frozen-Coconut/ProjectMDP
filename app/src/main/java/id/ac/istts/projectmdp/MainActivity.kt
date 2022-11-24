17de0d1905303c48bf40ed38c9809a980df9d133
Juan Alexanov <ajajuan10@gmail.com>
2022-11-24 21:42:40 +0700

    cobain bisa ga
    ini pakek auth

--- app/src/main/java/id/ac/istts/projectmdp/MainActivity.kt
+++ app/src/main/java/id/ac/istts/projectmdp/MainActivity.kt
@@ -3,19 +3,57 @@ package id.ac.istts.projectmdp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import id.ac.istts.projectmdp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
 
         binding.mainBtnToRegister.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
+        }
+        binding.mainBtnLogin.setOnClickListener {
+            val email = binding.mainTxtEmail.text.toString()
+            val password = binding.mainTxtPassword.text.toString()
+
+            if(email.isEmpty()){
+                binding.mainTxtEmail.error = "email harus diisi"
+                binding.mainTxtEmail.requestFocus()
+                return@setOnClickListener
+            }
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.mainTxtEmail.error = "Email tidak valid"
+                binding.mainTxtEmail.requestFocus()
+                return@setOnClickListener
+            }
+            if(password.isEmpty()){
+                binding.mainTxtPassword.error = "email harus diisi"
+                binding.mainTxtPassword.requestFocus()
+                return@setOnClickListener
+            }
+            loginauth(email, password)
+        }
+    }
+    private fun loginauth(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) {
+            if(it.isSuccessful){
+                Toast.makeText(this, "Login Berhasil Welcom $email", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity2::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/id/ac/istts/projectmdp/RegisterActivity.kt
+++ app/src/main/java/id/ac/istts/projectmdp/RegisterActivity.kt
@@ -1,17 +1,23 @@
 package id.ac.istts.projectmdp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
+import android.widget.Button
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import id.ac.istts.projectmdp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityRegisterBinding
-    private lateinit var users: ArrayList<User>
+    lateinit var cokcokcok: Button
+    lateinit var binding: ActivityRegisterBinding
+    lateinit var auth: FirebaseAuth
+//    private lateinit var users: ArrayList<User>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,38 +25,83 @@ class RegisterActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        val ref = Connection.users
+        auth = FirebaseAuth.getInstance()
 
-        ref.addValueEventListener(object: ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                users = snapshot.value as ArrayList<User>
-                Log.e("firebase", "Value is: ${snapshot.value}")
-            }
-            override fun onCancelled(error: DatabaseError) {
-                Log.e("firebase", "Failed to read value!", error.toException())
-            }
-        })
-
-        val task = ref.get()
-        task.addOnSuccessListener {
-            users = it.value as ArrayList<User>
-            Log.e("firebase", "Value is: ${it.value}")
-        }.addOnFailureListener {
-            Log.e("firebase", "Failed to read value!", it)
+        cokcokcok = findViewById(R.id.btn_register)
+        binding.tvToLogin.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
         }
 
-        binding.registerBtnRegister.setOnClickListener {
-            if (binding.registerTxtUsername.text.isEmpty() || binding.registerTxtPassword.text.isEmpty() || binding.registerTxtName.text.isEmpty()) {
-                Toast.makeText(this, "Input fields must not be empty!", Toast.LENGTH_SHORT).show()
-            } else {
-                val user = User(binding.registerTxtUsername.text.toString(), binding.registerTxtPassword.text.toString(), binding.registerTxtName.text.toString())
-                users.add(user)
-                ref.setValue(users)
-            }
-        }
+        binding.btnRegister.setOnClickListener {
+            val email = binding.edtEmailRegister.text.toString()
+            val password = binding.edtPasswordRegister.text.toString()
 
-        binding.registerBtnToLogin.setOnClickListener {
-            finish()
+            if(email.isEmpty()){
+                binding.edtEmailRegister.error = "email harus diisi"
+                binding.edtEmailRegister.requestFocus()
+                return@setOnClickListener
+            }
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.edtEmailRegister.error = "Email tidak valid"
+                binding.edtEmailRegister.requestFocus()
+                return@setOnClickListener
+            }
+            if(password.isEmpty()){
+                binding.edtPasswordRegister.error = "email harus diisi"
+                binding.edtPasswordRegister.requestFocus()
+                return@setOnClickListener
+            }
+            if (password.length < 6) {
+                binding.edtPasswordRegister.error = "Password Minimal 6 Karakter"
+                binding.edtPasswordRegister.requestFocus()
+                return@setOnClickListener
+            }
+            registerauth(email, password)
+        }
+//        val ref = Connection.users
+//
+//        ref.addValueEventListener(object: ValueEventListener {
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                users = snapshot.value as ArrayList<User>
+//                Log.e("firebase", "Value is: ${snapshot.value}")
+//            }
+//            override fun onCancelled(error: DatabaseError) {
+//                Log.e("firebase", "Failed to read value!", error.toException())
+//            }
+//        })
+//
+//        val task = ref.get()
+//        task.addOnSuccessListener {
+//            users = it.value as ArrayList<User>
+//            Log.e("firebase", "Value is: ${it.value}")
+//        }.addOnFailureListener {
+//            Log.e("firebase", "Failed to read value!", it)
+//        }
+//
+//        binding.registerBtnRegister.setOnClickListener {
+//            if (binding.registerTxtUsername.text.isEmpty() || binding.registerTxtPassword.text.isEmpty() || binding.registerTxtName.text.isEmpty()) {
+//                Toast.makeText(this, "Input fields must not be empty!", Toast.LENGTH_SHORT).show()
+//            } else {
+//                val user = User(binding.registerTxtUsername.text.toString(), binding.registerTxtPassword.text.toString(), binding.registerTxtName.text.toString())
+//                users.add(user)
+//                ref.setValue(users)
+//            }
+//        }
+//
+//        binding.registerBtnToLogin.setOnClickListener {
+//            finish()
+//        }
+    }
+    private fun registerauth(email: String, password: String){
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this) { 
+            if(it.isSuccessful){
+                Toast.makeText(this, "Register Berhasil", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
