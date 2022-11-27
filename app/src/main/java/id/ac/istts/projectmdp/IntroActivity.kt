3b8c40e579c6ba7d3e1a2b999b43e35b0d2c9252
Juan Alexanov <ajajuan10@gmail.com>
2022-11-28 01:56:40 +0700

    login register sementara design nya bgini , nanti final nya terakhir aja
    PUTANG INA MO

--- app/src/main/java/id/ac/istts/projectmdp/IntroActivity.kt
+++ app/src/main/java/id/ac/istts/projectmdp/IntroActivity.kt
@@ -7,16 +7,22 @@ import android.widget.Button
 
 class IntroActivity : AppCompatActivity() {
     private lateinit var btn_signup : Button
+    private lateinit var btn_login : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
         btn_signup = findViewById(R.id.btn_signup)
+        btn_login = findViewById(R.id.btn_login)
 
         btn_signup.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
             
+        }
+        btn_login.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
