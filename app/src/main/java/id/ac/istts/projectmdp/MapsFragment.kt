39648cb7b23065d09961d645a919874cfa832e1e
Frozen-Coconut <83059636+Frozen-Coconut@users.noreply.github.com>
2022-12-11 18:11:01 +0700

    Coba Map (Jalan)
    Jangan lupa masukkin API KEY ke local.properties sebelum coba jalanin

--- app/src/main/java/id/ac/istts/projectmdp/MapsFragment.kt
+++ app/src/main/java/id/ac/istts/projectmdp/MapsFragment.kt
@@ -6,11 +6,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.CameraPosition
 
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
@@ -26,9 +24,9 @@ class MapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val ISTTS = LatLng(-7.291290184677537, 112.75882726352205)
+        googleMap.addMarker(MarkerOptions().position(ISTTS).title("Institut Sains dan Teknologi Terpadu Surabaya"))
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(ISTTS))
     }
 
     override fun onCreateView(
