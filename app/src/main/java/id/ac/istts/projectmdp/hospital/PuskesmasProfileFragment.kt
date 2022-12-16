dd4bb976048a31ef23ee94a3b04b4c5a329323c1
weifuk <weifuk678@gmail.com>
2022-12-16 11:12:10 +0700

    Profile Puskesmas Belum jadi

--- app/src/main/java/id/ac/istts/projectmdp/hospital/PuskesmasProfileFragment.kt
+++ app/src/main/java/id/ac/istts/projectmdp/hospital/PuskesmasProfileFragment.kt
@@ -5,9 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import id.ac.istts.projectmdp.R
+import id.ac.istts.projectmdp.databinding.FragmentPuskesmasProfileBinding
 
 class PuskesmasProfileFragment : Fragment() {
+
+    lateinit var tpNama:EditText
+    lateinit var tpAlamat:EditText
+    lateinit var tpNoHp:EditText
+    lateinit var tpEmail:EditText
+    lateinit var btnEdit:Button
+
+    var edit = false
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,6 +30,40 @@ class PuskesmasProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // ...
+        tpNama = view.findViewById(R.id.profile_txtNama)
+        tpAlamat = view.findViewById(R.id.profile_txtAlamat)
+        tpNoHp = view.findViewById(R.id.profile_txtNoHP)
+        tpEmail = view.findViewById(R.id.profile_txtEmail)
+
+        btnEdit = view.findViewById(R.id.btnEdit)
+
+        btnEdit.setOnClickListener {
+            if (edit) {
+                edit = false
+                //Save data
+
+                setOnEdit()
+            }
+            else {
+                edit = true
+
+                setOnEdit()
+            }
+        }
+    }
+
+    fun setOnEdit() {
+        if (edit) {
+            tpNama.isEnabled = true
+            tpAlamat.isEnabled = true
+            tpNoHp.isEnabled = true
+            tpEmail.isEnabled = true
+        }
+        else {
+            tpNama.isEnabled = false
+            tpAlamat.isEnabled = false
+            tpNoHp.isEnabled = false
+            tpEmail.isEnabled = false
+        }
     }
 }
