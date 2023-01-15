687d8dca4bdee48f2bd70822294d3f9975dc991e
weifuk <weifuk678@gmail.com>
2023-01-15 12:03:59 +0700

    Privacy Policy

---
+++ api/resources/views/privacypolicy.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Privacy Policy</title>
+</head>
+<body>
+    <h1>Semua data yang kami ambil, dan kami gunakan akan hanya digunakan di dalam App dan tidak akan disalahgunakan.</h1>
+</body>
+</html>
